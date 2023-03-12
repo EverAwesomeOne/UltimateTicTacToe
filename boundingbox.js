cd790caddd1497edbd11b7a24618c1bea00a3671
Chloe D <owlgirlpnw@gmail.com>
2023-03-11 19:13:26 -0800

    refactored code

--- boundingbox.js
+++ boundingbox.js
@@ -1,4 +1,14 @@
 class BoundingBox {
+    cell0;
+    cell1;
+    cell2;
+    cell3;
+    cell4;
+    cell5;
+    cell6;
+    cell7;
+    cell8;
+
     constructor(x, y, width, height) {
         Object.assign(this, { x, y, width, height });
 
@@ -12,5 +22,5 @@ class BoundingBox {
         if (this.right > oth.left && this.left < oth.right && this.top < oth.bottom && this.bottom > oth.top) return true;
         return false;
     };
-};
+}
 
--- pages/tictactoe.js
+++ pages/tictactoe.js
@@ -2,30 +2,61 @@ class TicTacToe {
     constructor(game) {
         this.game = game;
 
+        // first move boolean
+        this.firstMove = false;
+
         // duplicate code
         this.rowX = (PARAMS.CANVAS_WIDTH - 600) / 2;
         this.rowY = ((PARAMS.CANVAS_HEIGHT - 600) / 2) + 20;  // account for HUD height
 
         // bounding boxes
-        this.mouseBB = new BoundingBox(0,0,1,1);
-        this.boardBoundingBoxes();
+        this.mouseBB = new BoundingBox(0, 0, 1, 1);
+        this.createBoard();
     }
 
-    boardBoundingBoxes() {
+    createBoard() {
         // row 1
-        this.cell0 = new BoundingBox(this.rowX, this.rowY,200,200);
-        this.cell1 = new BoundingBox(this.rowX + 200, this.rowY,200,200);
-        this.cell2 = new BoundingBox(this.rowX + 400, this.rowY,200,200);
+        this.cell0 = new BoundingBox(this.rowX, this.rowY, 200, 200);
+        this.cell1 = new BoundingBox(this.rowX + 200, this.rowY, 200, 200);
+        this.cell2 = new BoundingBox(this.rowX + 400, this.rowY, 200, 200);
 
         // row 2
-        this.cell3 = new BoundingBox(this.rowX, this.rowY + 200,200,200);
-        this.cell4 = new BoundingBox(this.rowX + 200, this.rowY + 200,200,200);
-        this.cell5 = new BoundingBox(this.rowX + 400, this.rowY + 200,200,200);
+        this.cell3 = new BoundingBox(this.rowX, this.rowY + 200, 200, 200);
+        this.cell4 = new BoundingBox(this.rowX + 200, this.rowY + 200, 200, 200);
+        this.cell5 = new BoundingBox(this.rowX + 400, this.rowY + 200, 200, 200);
 
         // row 3
-        this.cell6 = new BoundingBox(this.rowX, this.rowY + 400,200,200);
-        this.cell7 = new BoundingBox(this.rowX + 200, this.rowY + 400,200,200);
-        this.cell8 = new BoundingBox(this.rowX + 400, this.rowY + 400,200,200);
+        this.cell6 = new BoundingBox(this.rowX, this.rowY + 400, 200, 200);
+        this.cell7 = new BoundingBox(this.rowX + 200, this.rowY + 400, 200, 200);
+        this.cell8 = new BoundingBox(this.rowX + 400, this.rowY + 400, 200, 200);
+
+        // inner cells
+        this.createInnerCells(this.cell0);
+        this.createInnerCells(this.cell1);
+        this.createInnerCells(this.cell2);
+        this.createInnerCells(this.cell3);
+        this.createInnerCells(this.cell4);
+        this.createInnerCells(this.cell5);
+        this.createInnerCells(this.cell6);
+        this.createInnerCells(this.cell7);
+        this.createInnerCells(this.cell8);
+    }
+
+    createInnerCells(cell) {
+        // row 1
+        cell.cell0 = new BoundingBox(cell.x, cell.y, 200 / 3, 200 / 3);
+        cell.cell1 = new BoundingBox(cell.x + 200 / 3, cell.y, 200 / 3, 200 / 3);
+        cell.cell2 = new BoundingBox(cell.x + 200 - 200 / 3, cell.y, 200 / 3, 200 / 3);
+
+        // row 2
+        cell.cell3 = new BoundingBox(cell.x, cell.y + 200, 200 / 3, 200 / 3);
+        cell.cell4 = new BoundingBox(cell.x + 200 / 3, cell.y + 200 / 3, 200 / 3, 200 / 3);
+        cell.cell5 = new BoundingBox(cell.x + 200 - 200 / 3, cell.y + 200 / 3, 200 / 3, 200 / 3);
+
+        // row 3
+        cell.cell6 = new BoundingBox(cell.x, cell.y + 400, 200 / 3, 200 / 3);
+        cell.cell7 = new BoundingBox(cell.x + 200 / 3, cell.y + 200 - 200 / 3, 200 / 3, 200 / 3);
+        cell.cell8 = new BoundingBox(cell.x + 200 - 200 / 3, cell.y + 200 - 200 / 3, 200 / 3, 200 / 3);
     }
 
     update() {
@@ -42,25 +73,15 @@ class TicTacToe {
 
             if (this.mouseBB.collide(this.cell0)) {
 
-            }
+            } else if (this.mouseBB.collide(this.cell1)) {
 
-            else if (this.mouseBB.collide(this.cell1)) {
+            } else if (this.mouseBB.collide(this.cell2)) {
 
-            }
+            } else if (this.mouseBB.collide(this.cell3)) {
 
-            else if (this.mouseBB.collide(this.cell2)) {
+            } else if (this.mouseBB.collide(this.cell4)) {
 
-            }
-
-            else if (this.mouseBB.collide(this.cell3)) {
-
-            }
-
-            else if (this.mouseBB.collide(this.cell4)) {
-
-            }
-
-            else if (this.mouseBB.collide(this.cell5)) {
+            } else if (this.mouseBB.collide(this.cell5)) {
 
             }
 
@@ -70,68 +91,51 @@ class TicTacToe {
 
         // update mouse movement
         if (this.game.mouse) {
-            this.mouseBB = new BoundingBox(this.game.mouse.x, this.game.mouse.y,1,1);
+            this.mouseBB = new BoundingBox(this.game.mouse.x, this.game.mouse.y, 1, 1);
         }
     }
 
     draw(ctx) {
         this.drawBoard(ctx);
-        //this.playAnywhere(ctx);
-        //this.playInCell(ctx, this.cell5);
-        this.drawCells(ctx);
     }
 
     // draw main board grid
     drawBoard(ctx) {
+        // draw big board
         setCustomStroke(ctx, "black");
-
         ctx.beginPath();
-
         // left vertical line
         ctx.moveTo(this.rowX + 200, this.rowY);
         ctx.lineTo(this.rowX + 200, this.rowY + 600);
         // right vertical line
         ctx.moveTo(this.rowX + 400, this.rowY);
         ctx.lineTo(this.rowX + 400, this.rowY + 600);
-
         // top horizontal line
         ctx.moveTo(this.rowX, this.rowY + 200);
         ctx.lineTo(this.rowX + 600, this.rowY + 200);
-
         // bottom horizontal line
         ctx.moveTo(this.rowX, this.rowY + 400);
         ctx.lineTo(this.rowX + 600, this.rowY + 400);
-
         ctx.stroke();
-    }
 
-    // player help
-    playAnywhere(ctx) {
-        let playHereBB = new BoundingBox(this.rowX, this.rowY,600,600);
-        setRainbowStroke(ctx, playHereBB);
-        ctx.strokeRect(this.rowX, this.rowY,600,600);
-    }
-
-    playInCell(ctx, cell) {
-        let playHereBB = new BoundingBox(cell.x, cell.y,200,200);
-        setRainbowStroke(ctx, playHereBB);
-        ctx.strokeRect(cell.x, cell.y,200,200);
+        // draw inner cells of big board
+        this.drawCells(ctx);
     }
 
     drawCells(ctx) {
-        this.drawCellGrid(ctx, this.cell0, "pink");
-        this.drawCellGrid(ctx, this.cell1, "red");
-        this.drawCellGrid(ctx, this.cell2, "orange");
-        this.drawCellGrid(ctx, this.cell3, "maroon");
-        this.drawCellGrid(ctx, this.cell4, "green");
-        this.drawCellGrid(ctx, this.cell5, "teal");
-        this.drawCellGrid(ctx, this.cell6, "navy");
-        this.drawCellGrid(ctx, this.cell7, "blue");
-        this.drawCellGrid(ctx, this.cell8, "purple");
+        this.drawInnerCellGrid(ctx, this.cell0, "pink");
+        this.drawInnerCellGrid(ctx, this.cell1, "red");
+        this.drawInnerCellGrid(ctx, this.cell2, "orange");
+        this.drawInnerCellGrid(ctx, this.cell3, "maroon");
+        this.drawInnerCellGrid(ctx, this.cell4, "green");
+        this.drawInnerCellGrid(ctx, this.cell5, "teal");
+        this.drawInnerCellGrid(ctx, this.cell6, "navy");
+        this.drawInnerCellGrid(ctx, this.cell7, "blue");
+        this.drawInnerCellGrid(ctx, this.cell8, "purple");
 
     }
 
-    drawCellGrid(ctx, cell, color) {
+    drawInnerCellGrid(ctx, cell, color) {
         setCustomStroke(ctx, color);
 
         ctx.beginPath();
@@ -154,39 +158,16 @@ class TicTacToe {
         ctx.stroke();
     }
 
-    boardCell0() {
-
+    // player help
+    playAnywhere(ctx) {
+        let playHereBB = new BoundingBox(this.rowX, this.rowY, 600, 600);
+        setRainbowStroke(ctx, playHereBB);
+        ctx.strokeRect(this.rowX, this.rowY, 600, 600);
     }
 
-    boardCell1() {
-
-    }
-
-    boardCell2() {
-
-    }
-
-    boardCell3() {
-
-    }
-
-    boardCell4() {
-
-    }
-
-    boardCell5() {
-
-    }
-
-    boardCell6() {
-
-    }
-
-    boardCell7() {
-
-    }
-
-    boardCell8() {
-
+    playInCell(ctx, cell, dimension) {
+        let playHereBB = new BoundingBox(cell.x, cell.y, dimension, dimension);
+        setRainbowStroke(ctx, playHereBB);
+        ctx.strokeRect(cell.x, cell.y, dimension, dimension);
     }
 }
